826d516b3a092f64404a07965e9793aecd9f9fe2
aspyring <abhaychemist@gmail.com>
2023-10-09 15:14:32 +0800

    Update Review Question 1.py
    Changed from being able to specify the number of numbers to find the average of to only 5.

--- Review Question 1.py
+++ Review Question 1.py
@@ -1,11 +1,10 @@
 #Initialises the total
 total = 0
-noOfNums = int(input("Enter amount of numbers to calculate average: "))
 #Loop to input Numbers
-for x in range(noOfNums):
+for x in range(5):
     num = int(input("Enter number " + str(x + 1) + ": "))
     total = total + num
 
 #Calculates average of the specified number of numbers and prints it.
-avg = total / noOfNums
-print("The average of the " + str(noOfNums) + " numbers is: " + str(avg))
+avg = total / 5
+print("The average of the 5 numbers is: " + str(avg))
