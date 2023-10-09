aad03e64de62d9be0bd8d84bdbfdf7c42069cb46
aspyring <abhaychemist@gmail.com>
2023-10-09 15:44:28 +0800

    New Exercise
    Convert pseudocode from Lab 1 class to Python, Calculate the Circumference and Area of a circle
    given a radius input by the user

---
+++ Circumference and Area of a circle.py
@@ -0,0 +1,7 @@
+import math
+r = int(input("Input Radius: "))
+C = round((math.pi * 2 * r), 2)
+A = round((math.pi * r * r), 2)
+print("Circumference:", C , "\nArea:", A)
+print("Circumference: " + str(C) + "\nArea: " + str(A))
+print(f"Circumference: {C} \nArea: {A}")
--- Review Question 1.py
+++ Review Question 1.py
@@ -1,10 +1,9 @@
-#Initialises the total
+#Initialises the total variable
 total = 0
 #Loop to input Numbers
 for x in range(5):
     num = int(input("Enter number " + str(x + 1) + ": "))
     total = total + num
-
 #Calculates average of the specified number of numbers and prints it.
 avg = total / 5
 print("The average of the 5 numbers is: " + str(avg))
